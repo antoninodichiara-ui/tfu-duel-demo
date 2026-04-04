2dcf85a01f25eaf9791b2d999bfe8b0e968e2d45
antoninodichiara-ui <antonino.dichiara@googlemail.com>
2026-04-04 15:00:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-.
+let score = 0;
+
+function compare(playerValue) {
+  let enemyValue = Math.floor(Math.random() * 100);
+
+  if (playerValue > enemyValue) {
+    setResult('win');
+  } else if (playerValue < enemyValue) {
+    setResult('lose');
+  } else {
+    setResult('draw');
+  }
+}
+
+function setResult(result) {
+  let text = document.getElementById("resultText");
+
+  if (result === 'win') {
+    text.innerText = "You Win!";
+    score++;
+  } else if (result === 'lose') {
+    text.innerText = "You Lose!";
+  } else {
+    text.innerText = "Draw!";
+  }
+
+  document.getElementById("score").innerText = score;
+}
